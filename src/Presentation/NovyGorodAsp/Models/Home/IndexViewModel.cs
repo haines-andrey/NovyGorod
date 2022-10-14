7be74ad713dfa372bac4d45b58d2a6dea7fd011f
haines_andrey <haines.andrey@gmail.com>
2022-10-15 02:18:04 +0300

    index view was refactored.

--- src/Presentation/NovyGorodAsp/Models/Home/IndexViewModel.cs
+++ src/Presentation/NovyGorodAsp/Models/Home/IndexViewModel.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections.Generic;
 using NovyGorod.Application.Contracts.Posts.Dto;
 using NovyGorod.Application.Contracts.Posts.Requests;
+using NovyGorod.Domain.Models.Posts;
+using NovyGorodAsp.Models.Shared;
 
 namespace NovyGorodAsp.Models.Home;
 
 public class IndexViewModel
 {
+    private const int PageIndex = 0;
+    private const int PageSize = 6;
+
     public IReadOnlyCollection<PostListDto> LastProjects { get; init; }
 
     public IReadOnlyCollection<PostListDto> LastTheatrePosts { get; init; }
 
     public IReadOnlyCollection<PostListDto> LastSchoolPosts { get; init; }
 
+    public PostListContainerViewModel LastProjectsContainer => new() { Posts = LastProjects };
+    public PostListContainerViewModel LastTheatrePostsContainer => new() { Posts = LastProjects };
+    public PostListContainerViewModel LastSchoolPostsContainer => new() { Posts = LastProjects };
+
+    public SearchPostsRequest SearchProjectsRequest => new()
+    {
+        Type = PostType.Project, PageIndex = PageIndex, PageSize = PageSize,
+    };
     
+    public SearchPostsRequest SearchTheatrePostsRequest => new()
+    {
+        Type = PostType.Theatre, PageIndex = PageIndex, PageSize = PageSize,
+    };
+    
+    public SearchPostsRequest SearchSchoolPostsRequest => new()
+    {
+        Type = PostType.School, PageIndex = PageIndex, PageSize = PageSize,
+    };
 }
