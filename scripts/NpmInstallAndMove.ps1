d994dc4d8394dfdb5b2e271c9c9b6a70c41823f7
haines_andrey <haines.andrey@gmail.com>
2022-10-22 02:31:33 +0300

    wwwroot/lib improvement. style.scss

---
+++ scripts/NpmInstallAndMove.ps1
@@ -0,0 +1,11 @@
+﻿. "$PSScriptRoot\Definitions.ps1"
+
+Set-Location $StartupProject
+echo "npm install..."
+npm install
+
+echo "remove old modules from wwwroot\lib..."
+Remove-Item "$StartupProject\wwwroot\lib\*" -Recurse
+
+echo "move new modules..."
+Move-Item -Path "$StartupProject\node_modules\*" -Destination "$StartupProject\wwwroot\lib"
