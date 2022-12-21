30a3a35be9dffbeff62cabde7e3490b7ba6a172f
haines_andrey <haines.andrey@gmail.com>
2022-12-21 23:25:44 +0300

    footer view was added.

---
+++ src/Presentation/NovyGorodAsp/Models/Shared/FooterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NovyGorodAsp.Models.Shared;
+
+public class FooterViewModel
+{
+    public string GetCopyrightString(string companyName)
+    {
+        return $"{companyName} \u00a9 {DateTime.Today.Year}";
+    }
+}
