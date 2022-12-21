57a861ab28c14dc2a9c102eecd03117d20b96a99
haines_andrey <haines.andrey@gmail.com>
2022-12-21 23:21:46 +0300

    layout was fixed

--- src/Presentation/NovyGorodAsp/wwwroot/js/site.js
+++ src/Presentation/NovyGorodAsp/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let headerHeight = $("header").height()
+$("main").css("margin-top", headerHeight + 25)
