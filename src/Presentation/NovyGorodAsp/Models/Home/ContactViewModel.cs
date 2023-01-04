c268f3f09326cd5c377f0c862503a06da713d542
haines_andrey <haines.andrey@gmail.com>
2023-01-05 02:02:43 +0300

    contacts view was done.

---
+++ src/Presentation/NovyGorodAsp/Models/Home/ContactViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace NovyGorodAsp.Models.Home;
+
+public class ContactViewModel
+{
+    public string Url { get; init; }
+
+    public string IconName { get; init; }
+
+    public string Title { get; init; }
+
+    public string Info { get; init; }
+
+    public bool OpenLinkInNewPage { get; init; }
+}
--- src/Presentation/NovyGorodAsp/Models/Home/ContactsViewModel.cs
+++ src/Presentation/NovyGorodAsp/Models/Home/ContactsViewModel.cs
@@ -2,5 +2,37 @@
 
 public class ContactsViewModel
 {
-    
+    public ContactViewModel Phone { get; } = new()
+    {
+        Url = "tel:+375(29)652-31-52",
+        Title = "Телефон",
+        Info = "+375 (29) 652-31-52",
+        IconName = "bi-phone"
+    };
+
+    public ContactViewModel Mail { get; } = new()
+    {
+        Url = "mailto:lud.konopl.work@gmail.com",
+        Title = "Почта",
+        Info = "lud.konopl.work@gmail.com",
+        IconName = "bi-envelope"
+    };
+
+    public ContactViewModel Office { get; } = new()
+    {
+        Url = "https://yandex.ru/maps?pt=27.604851,53.917506&z=16&l=map",
+        Title = "Офис",
+        Info = "г.&nbsp;Минск, ул.&nbsp;Сурганова, д.&nbsp;2, оф.&nbsp;32",
+        IconName = "bi-geo-alt",
+        OpenLinkInNewPage = true,
+    };
+
+    public ContactViewModel Instagram { get; } = new()
+    {
+        Url = "tel:+375(29)652-31-52",
+        Title = "Инстаграм",
+        Info = "@novygorod.by",
+        IconName = "bi-instagram",
+        OpenLinkInNewPage = true,
+    };
 }
