8ed9f341fdbcb8d2c73abf15ac840d803d8e2827
haines_andrey <haines.andrey@gmail.com>
2023-01-07 00:03:23 +0300

    exception handling view was added.

---
+++ src/Presentation/NovyGorodAsp/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NovyGorodAsp.Models;
+
+namespace NovyGorodAsp.Controllers;
+
+public class ErrorController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new ErrorViewModel());
+    }
+}
