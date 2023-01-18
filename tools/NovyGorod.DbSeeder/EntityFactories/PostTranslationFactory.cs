24ec9062034ebc2e2356b0577218ee508d82cada
haines_andrey <haines.andrey@gmail.com>
2023-01-18 22:22:02 +0300

    db seeder was improved: caught case when media data url already exists in db

--- tools/NovyGorod.DbSeeder/EntityFactories/PostTranslationFactory.cs
+++ tools/NovyGorod.DbSeeder/EntityFactories/PostTranslationFactory.cs
@@ -1,6 +1,8 @@
 ﻿using NovyGorod.Common.Extensions;
 using NovyGorod.DbSeeder.Dtos;
 using NovyGorod.DbSeeder.Services;
+using NovyGorod.Domain.EntityAccess;
+using NovyGorod.Domain.EntityAccess.Queries;
 using NovyGorod.Domain.Models;
 using NovyGorod.Domain.Models.Posts;
 
@@ -8,11 +10,14 @@ namespace NovyGorod.DbSeeder.EntityFactories;
 
 internal class PostTranslationFactory : IEntityFactory<PostTranslation, PostDto>
 {
+    private readonly IEntityAccessService<MediaData> _mediaDataAccessService;
     private readonly IDefaultDataService _defaultDataService;
 
     public PostTranslationFactory(
+        IEntityAccessService<MediaData> mediaDataAccessService,
         IDefaultDataService defaultDataService)
     {
+        _mediaDataAccessService = mediaDataAccessService;
         _defaultDataService = defaultDataService;
     }
 
@@ -22,21 +27,42 @@ internal class PostTranslationFactory : IEntityFactory<PostTranslation, PostDto>
         {
             Title = dto.Title,
             Summary = dto.Summary,
-            Preview = CreatePreview(dto),
-            Video = dto.VideoUrl.IsNullOrEmpty() ? null : CreateVideo(dto),
+            Preview = await GetOrCreatePreview(dto),
+            Video = dto.VideoUrl.IsNullOrEmpty() ? null : await CreateVideo(dto),
             LanguageId = await _defaultDataService.GetLanguageId(),
         };
 
         return translation;
     }
 
-    private MediaData CreatePreview(PostDto dto)
+    private async Task<MediaData> GetOrCreatePreview(PostDto dto)
     {
-        return new MediaData {Type = MediaDataType.Image, IsLocal = true, Url = dto.PreviewImageFilePath};
+        var existing = await GetExisingMediaData(dto.PreviewImageFilePath);
+        
+        return existing ?? new MediaData {Type = MediaDataType.Image, IsLocal = true, Url = dto.PreviewImageFilePath};
     }
     
-    private MediaData CreateVideo(PostDto dto)
+    private async Task<MediaData> CreateVideo(PostDto dto)
     {
-        return new MediaData {Type = MediaDataType.Video, IsLocal = false, Url = dto.VideoUrl};
+        var existing = await GetExisingMediaData(dto.VideoUrl);
+        
+        return existing ?? new MediaData {Type = MediaDataType.Video, IsLocal = false, Url = dto.VideoUrl};
+    }
+
+    private Task<MediaData> GetExisingMediaData(string url)
+    {
+        var query = new MediaDataQueryParams {Url = url};
+
+        return _mediaDataAccessService.GetSingleOrDefault(query);
+    }
+
+    private class MediaDataQueryParams : BaseQueryParameters<MediaData>
+    {
+        public string Url { get; set; }
+
+        protected override void AddFilters()
+        {
+            Filter(x => x.Url == Url);
+        }
     }
 }
