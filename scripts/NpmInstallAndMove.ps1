8090a9443dde2bca97edd4ef1c2e7f7cc045460c
haines_andrey <haines.andrey@gmail.com>
2023-05-21 04:27:56 +0300

    fixes.

--- scripts/NpmInstallAndMove.ps1
+++ scripts/NpmInstallAndMove.ps1
@@ -5,9 +5,11 @@ echo "npm install..."
 npm install
 
 echo "remove old modules from wwwroot\lib..."
-$directory = "$StartupProject\wwwroot\lib\*"
+$directory = "$StartupProject\wwwroot\lib"
 if (Test-Path $directory) {
-    Remove-Item  -Recurse
+    Remove-Item "$directory\*" -Recurse
+} else {
+    New-Item -ItemType "directory" $directory
 }
 
 echo "move new modules..."
