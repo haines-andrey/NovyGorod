d28c7d6f6c5fd1a2a872baa2b52190ea19fef652
Andrey Haines <94743525+haines-andrey@users.noreply.github.com>
2023-06-29 04:36:31 +0300

    application version was added in footer.

--- src/Presentation/NovyGorodAsp/Models/Shared/FooterViewModel.cs
+++ src/Presentation/NovyGorodAsp/Models/Shared/FooterViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace NovyGorodAsp.Models.Shared;
 
@@ -7,5 +8,12 @@ public class FooterViewModel
     public string GetCopyrightString(string companyName)
     {
         return $"{companyName} \u00a9 {DateTime.Today.Year}";
+    }
+    
+    public string GetApplicationVersionString()
+    {
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+
+        return version is not null ? $"v{version.Major}.{version.Minor}" : null;
     }
 }
