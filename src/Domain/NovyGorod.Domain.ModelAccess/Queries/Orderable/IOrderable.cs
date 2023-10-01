16fc77d11ab5363d77493afe339dc286d4973af7
haines_andrey <haines.andrey@gmail.com>
2023-10-02 00:45:11 +0300

    IOrderable and IThenOrderable are fixed: generic type constraint was deleted.

--- src/Domain/NovyGorod.Domain.ModelAccess/Queries/Orderable/IOrderable.cs
+++ src/Domain/NovyGorod.Domain.ModelAccess/Queries/Orderable/IOrderable.cs
@@ -3,7 +3,6 @@
 namespace NovyGorod.Domain.ModelAccess.Queries.Orderable;
 
 public interface IOrderable<T>
-    where T : class
 {
     IThenOrderable<T> OrderBy<TKey>(Expression<Func<T, TKey>> keySelector);
 
