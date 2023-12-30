afbb7d4a8069beb12b1335ebaea10f82623f46c8
haines_andrey <haines.andrey@gmail.com>
2023-12-30 04:34:41 +0300

    user-secrets are deleted.

--- scripts/AddUserSecrets.ps1
+++
@@ -1,9 +0,0 @@
-﻿. "$PSScriptRoot\Definitions.ps1"
-. "$PSScriptRoot\UserSecrets.ps1"
-
-dotnet user-secrets init --project $StartupProject
-
-foreach ($key in $userSecrets.Keys)
-{
-    dotnet user-secrets set $key $userSecrets[$key] --project $StartupProject
-}
